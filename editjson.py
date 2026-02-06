e40b104cadf387f87117e065152a5b7179c3af36
Pascal von Fintel <pjvf17@gmail.com>
2026-02-06 12:27:25 -0500

    we hoping

--- editjson.py
+++ editjson.py
@@ -5,13 +5,21 @@ import re
 
 with open('strudel.json', mode='r+') as f:
     d = json.load(f)
-    print(d['bsrb2'])
-    construct = {}
+    things = []
+    for key in d:
+        for val in d[key]:
+            # print(val)
+            if val.endswith('A#0.wav'):
+                things.append(key)
+    for key in things:
+      # print(key)
+      construct = {}
 
-    for x in d['bsrb2']:
-       construct[x[x.rfind('/')+1:x.rfind('.')]] = x
-   
-    d['bsrb2'] = construct
+      for x in d[key]:
+         construct[x[x.rfind('/')+1:x.rfind('.')]] = x
+
+      d[key] = construct
+
     f.seek(0)
     json.dump(d, f)
     f.truncate()
